b340ec681edd030d615a41c7d788bd11ad9566d1
Patrick Boivin <pboivin@gmail.com>
2023-01-15 10:19:20 -0500

    Update home page

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,25 +1,30 @@
-@props(['title' => ''])
+@props([
+    'title' => '',
+    'siteName' => config('app.name'),
+])
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{!! $title ? "$title — " : '' !!}{!! config('app.name') !!}</title>
+        <title>{{ $title ? "$title — " : '' }}{{ config('app.name') }}</title>
         @vite(['resources/css/app.css'])
     </head>
     <body>
         <div class="flex flex-col min-h-screen">
             <header class="bg-black text-white">
-                <x-container class="py-4">
-                    <nav class="flex items-center">
-                        <div class="text-2xl">
-                            <a href="/">{!! config('app.name') !!}</a>
-                        </div>
+                <x-container>
+                    <nav class="main-nav flex items-center">
+                        @if ($siteName)
+                            <div class="text-2xl">
+                                <a class="text-inherit" href="/">{{ $siteName }}</a>
+                            </div>
+                        @endif
 
                         <ul class="ml-auto flex items-center space-x-2">
-                            <li><a href="{{ route('post.index') }}">Blog</a></li>
-                            <li><a href="{{ route('about') }}">About</a></li>
-                            <li><a href="{{ route('contact') }}">Contact</a></li>
+                            <li><a class="text-inherit" href="{{ route('post.index') }}">Blog</a></li>
+                            <li><a class="text-inherit" href="{{ route('about') }}">About</a></li>
+                            <li><a class="text-inherit" href="{{ route('contact') }}">Contact</a></li>
                         </ul>
                     </nav>
                 </x-container>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,12 @@
-<x-layout>
-    <x-container>
-        <div>
-            Featured posts...
-        </div>
+<x-layout :site-name="false">
+    <div class="site-intro flex items-center justify-center bg-black text-white">
+        <h1 class="text-[140px]">{{ config('app.name') }}</h1>
+    </div>
 
-        <div class="flex flex-col mt-4">
+    <x-container>
+        <h2 class="mt-8 text-xl">Featured posts</h2>
+
+        <div class="mt-8 flex flex-col">
             <a href="/blog/lorem">Lorem</a>
             <a href="/blog/ipsum">Ipsum</a>
             <a href="/blog/dolor">Dolor</a>
