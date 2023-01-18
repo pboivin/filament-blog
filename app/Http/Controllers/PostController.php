094684863902ec6270ac680b33639c01b49db0d1
Patrick Boivin <pboivin@gmail.com>
2023-01-18 09:39:02 -0500

    Cleanup post controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,25 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Category;
 use App\Models\Post;
 
 class PostController extends Controller
 {
     public function index()
     {
-        $categories = Category::all();
-        $posts = Post::published()->get();
-
-        if ($activeCategorySlug = request()->query('category')) {
-            $activeCategory = $categories->first(fn ($i) => $i->slug === $activeCategorySlug);
-        }
-
-        return view('post-index', [
-            'activeCategory' => $activeCategory ?? false,
-            'categories' => $categories,
-            'posts' => $posts,
-        ]);
+        return view('post-index');
     }
 
     public function show($slug)
